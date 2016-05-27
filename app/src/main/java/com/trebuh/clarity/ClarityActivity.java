11153ef1344146f6080503d6bd433d7461f5cf03
hubert <hubert.dworczynski@gmail.com>
2016-05-27 16:36:34 +0200

    initial commit

---
+++ app/src/main/java/com/trebuh/clarity/ClarityActivity.java
@@ -0,0 +1,13 @@
+package com.trebuh.clarity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClarityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clarity);
+    }
+}
